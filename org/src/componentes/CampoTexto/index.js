5d9b52b7b521c941061bb4e94b1216ddc1d4a5e3
Flaquima <flaquima@gmail.com>
2023-05-01 17:25:49 -0600

    Agregando el codigo de ejemplo de react ORG

---
+++ org/src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import "./CampoTexto.css"
+
+const CampoTexto = () => {
+    return <div>
+        <label>Nombre</label>
+        <input placeholder="Ingresar nombre" />
+    </div>
+}
+
+export default CampoTexto
