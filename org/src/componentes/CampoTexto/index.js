b68e97705460ec14a97e322f32dd96d2bf399c2d
Fatima <Fatima@LARANGEL-LAP.oradev.oraclecorp.com>
2023-05-02 20:58:45 -0600

    Cambios del martes

--- org/src/componentes/CampoTexto/index.js
+++ org/src/componentes/CampoTexto/index.js
@@ -1,9 +1,11 @@
 import "./CampoTexto.css"
 
-const CampoTexto = () => {
-    return <div>
-        <label>Nombre</label>
-        <input placeholder="Ingresar nombre" />
+const CampoTexto = (props) => {
+    console.log("Datos:", props.titulo)
+    const placeholderModificado = `${props.placeholder}...`
+    return <div className="campo-texto">
+        <label>{ props.titulo }</label>
+        <input placeholder={placeholderModificado} />
     </div>
 }
 
--- org/src/componentes/Formulario/Formulario.js
+++ org/src/componentes/Formulario/Formulario.js
@@ -6,7 +6,9 @@ const Formulario = () => {
     return <section className="formulario">
         <form>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto />
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
+            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
+            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
         </form>
     </section>
 }
